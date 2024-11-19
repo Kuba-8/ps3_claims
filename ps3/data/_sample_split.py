15f6ca9221ec0566635ffa21373465d7aa887a71
Kuba-8 <jakubfridrich8@gmail.com>
2024-11-19 14:08:54 +0000

    Created a function for the train/test spllit.

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -21,5 +21,10 @@ def create_sample_split(df, id_column, training_frac=0.8):
     pd.DataFrame
         Training data with sample column containing train/test split based on IDs.
     """
-
+    if df[id_column].dtype == np.object:
+        df[id_column] = df[id_column].apply(lambda x: sum(ord(char)) for char in x)
+        df['sample'] = df[id_column].apply(lambda x: 'train' if x % 100 < training_frac * 100 else 'test')
+    else:
+        df['sample'] = df[id_column].apply(lambda x: 'train' if x % 100 < training_frac * 100 else 'test')
     return df
+
